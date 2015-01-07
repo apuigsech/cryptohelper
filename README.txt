6a89b3376a1610eca6057420a692e829ef003f51
Albert Puigsech Galicia <albert@puigsech.com>
2015-01-07 02:07:18 +0100

    Initial commit

---
+++ README.txt
@@ -0,0 +1,12 @@
+# cryptohelper
+
+Generic Cryptography code utils used on matasano cryptography challenges (http://cryptopals.com/)
+
+## Installation
+
+	python setup.py install
+
+## Usage
+
+    import cryptohelper
+
---
+++ cryptohelper/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# Generic Cryptography code utils used on matasano cryptography challenges (http://cryptopals.com/)
+#
+# Copyright (c) 2015 - Albert Puigsech Galicia (albert@puigsech.com)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+from cryptohelper import *
+
+__title__ = 'cryptohelper'
+__description__ = 'Generic Cryptography code utils used on matasano cryptography challenges'
+__version__ = '1.0'
+__url__ = ''
+__author__ = 'Albert Puigsech Galicia'
+__author_email__ = 'albert@puigsech.com'
+__license__ = 'GPL 3.0'
+__copyright__ = 'Copyright (c) 2015 - Albert Puigsech Galicia'
