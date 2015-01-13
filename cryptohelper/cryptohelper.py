351d7a81df39ed1ecf74008437771a627af6c4a2
Albert Puigsech Galicia <albert@puigsech.com>
2015-01-13 01:17:30 +0100

    add many time pad statistical attack

--- cryptohelper/cryptohelper.py
+++ cryptohelper/cryptohelper.py
@@ -24,6 +24,8 @@
 
 import math
 import struct
+import itertools
+import operator
 from Crypto.Cipher import AES
 
 def strxor(a, b):
@@ -142,7 +144,7 @@ def keystream_block_CTR(blocklen, numblocks, nonce, key, prf, le_counter):
 	return ks
 
 def encrypt_block_CTR(pt, blocklen, nonce, key, prf, le_counter=False):
-	ks = keystream_block_CTR(blocklen, int(math.ceil(float(len(ct))/blocklen)), nonce, key, prf, le_counter)
+	ks = keystream_block_CTR(blocklen, int(math.ceil(float(len(pt))/blocklen)), nonce, key, prf, le_counter)
 	return strxor(ks, pt)
 
 
@@ -279,3 +281,38 @@ def oracle_ECB_decrypt(challenge, ctlen, blocksize, charset, prefix_len=0):
 		else:
 			break
 	return guess_pt[blocksize+prefix_align:]
+
+
+def combine_charset(charset):
+	comb = {}
+	for ch1, ch2 in list(itertools.combinations(charset, 2)):
+		comb.setdefault(ord(ch1)^ord(ch2), set()).add(ch1)
+		comb.setdefault(ord(ch1)^ord(ch2), set()).add(ch2)
+	return comb
+
+
+def keystream_from_many_time_pad(ct_list, freq):
+	ks = [{} for i in range(len(sorted(ct_list, key=len, reverse=True)[0]))]
+
+	comb = combine_charset(''.join(freq.keys()))
+
+	for ct1, ct2 in list(itertools.combinations(ct_list, 2)):
+		for i in range(min(len(ct1),len(ct2))):
+			mix = ord(ct1[i])^ord(ct2[i])
+			if comb.has_key(mix):
+				for ch in comb[mix]:
+					mix_1 = chr(ord(ct1[i])^ord(ch))
+					mix_2 = chr(ord(ct2[i])^ord(ch))
+					ks[i].setdefault(mix_1, 0)
+					ks[i].setdefault(mix_2, 0)
+					ks[i][mix_1] += freq[ch]
+					ks[i][mix_2] += freq[ch]
+
+	ks_str = ''
+	for k in ks:
+		if (len(k) > 0):
+			ks_str += sorted(k.items(), key=operator.itemgetter(1), reverse=True)[0][0]
+		else:
+			ks_str += "\x00"
+
+	return ks_str
