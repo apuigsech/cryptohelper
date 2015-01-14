2dda92f30ecacbcea872ae93f1a40e503361c13d
Albert Puigsech Galicia <albert@puigsech.com>
2015-01-14 20:25:04 +0100

    bugfixes

--- cryptohelper/cryptohelper.py
+++ cryptohelper/cryptohelper.py
@@ -33,16 +33,30 @@ mt_idx = 0
 
 
 def mt_init(seed):
+	global mt_matrix
 	global mt_idx
 
 	mt_idx = 0
+	mt_matrix = []
 	mt_matrix.append(seed & 0xffffffff)
 	for i in range(1,624):
 		mt_matrix.append(((1812433253 * (mt_matrix[i-1] ^ (mt_matrix[i-1]>>30)) + i)) & 0xffffffff)
 
 
+def mt_gen_numbers():
+	global mt_matrix
+
+	for i in range(624):
+		y = mt_matrix[i] = (mt_matrix[i] & 0x80000000) + (mt_matrix[(i+1)%264] & 0x7fffffff)
+		mt_matrix[i] = mt_matrix[(i+397)%624] ^ (y>>1)
+		if (y%2) != 0:
+			mt_matrix[i] = mt_matrix[i] ^ 2567483615
+
+
 def mt_next():
+	global mt_matrix
 	global mt_idx
+
 	if mt_idx == 0:
 		mt_gen_numbers()
 
@@ -55,14 +69,6 @@ def mt_next():
 	mt_idx = (mt_idx + 1)%624
 
 	return y
-
-
-def mt_gen_numbers():
-	for i in range(624):
-		y = mt_matrix[i] = (mt_matrix[i] & 0x80000000) + (mt_matrix[(i+1)%264] & 0x7fffffff)
-		mt_matrix[i] = mt_matrix[(i+397)%624] ^ (y>>1)
-		if (y%2) != 0:
-			mt_matrix[i] = mt_matrix[i] ^ 2567483615
 
 
 def strxor(a, b):
